e15116ef36ee98a5be64e2dd7f244fb6b76136b7
epassi <epassi@Emilio-Passis-MacBook-Pro.local>
2015-02-11 00:36:40 -0800

    Continued support for multiple pages.

--- tasks/responsify.js
+++ tasks/responsify.js
@@ -6,19 +6,13 @@ var done;
 
 module.exports = function ( grunt ) {
 	var _layouts = {};
-	// var _breakpoints = [];
 	var _htmlTemplate = grunt.file.read("resources/partials/layout.html");
 	var _cssTemplate = grunt.file.read("resources/partials/layout.css");
-	// var _html = "";
-	// var _css = "";
-
 
 	// Please see the Grunt documentation for more information regarding task
 	// creation: http://gruntjs.com/creating-tasks
 
   	grunt.registerMultiTask( 'responsify', 'generates a responsive prototype from layout comps/wireframes', function () {
-		// var pageTemplate = grunt.file.read("resources/partials/template.html");
-
 		// Force synchronous flow.
 		// Don't allow next grunt task to start until this one is finished.
 		done = this.async();
@@ -26,24 +20,12 @@ module.exports = function ( grunt ) {
 		// Get breakpoints.
 		resetLayouts();
 		grunt.file.recurse("layouts/", traverseFile);
-		// _breakpoints.sort(function(a,b){return a-b;});
 
 		// Create image slices out of the layouts.
 		async.each(Object.keys(_layouts), processLayout, onComplete);
-		// async.each(_breakpoints, processBreakpoint, onComplete);
-
-		// Generate the HTML file.
-		// _html = getLayoutHTML();
-		// _css = getLayoutCSS();
-		// pageTemplate = pageTemplate.replace(/{{styles}}/g, _css);
-		// pageTemplate = pageTemplate.replace(/{{maxBreakpoint}}/g, _breakpoints[_breakpoints.length-1]);
-		// pageTemplate = pageTemplate.replace(/{{html}}/g, _html);
-		// grunt.file.write("index.html", pageTemplate);
-
   	});
 
 	function resetLayouts() {
-		// _breakpoints = [];
 		_layouts = {};
 		grunt.file.delete("resources/img");
 	}
@@ -58,8 +40,6 @@ module.exports = function ( grunt ) {
 			function () {return i < breakpoints.length;},
 			function (callback) {
 				createSlices(title, breakpoints[i++], callback);
-				// processBreakpoint(title, breakpoints[i++], callback)
-				// createSlices(_breakpoints[i++], callback);
 			},
 			function (err) {
 				// Generate the HTML file.
@@ -76,33 +56,13 @@ module.exports = function ( grunt ) {
 		);
 	}
 
-	// function processBreakpoint(title, breakpoint, callbackBreakpointsComplete) {
-	// 	var i = 0;
-	// 	async.whilst(
-	// 		function () {return i < _breakpoints.length;},
-	// 		function (callback) {
-	// 			createSlices(_breakpoints[i++], callback);
-
-	// 		},
-	// 		function (err) {
-	// 			callbackBreakpointsComplete();
-	// 		}
-	// 	);
-	// }
-
 	function onComplete(err) {
 		grunt.log.writeln("everything done");
-		// done(true);
+		done(true);
 	}
 
 
 	function traverseFile(abspath, rootdir, subdir, filename) {
-		// var filenameBase = getFilenameBase(filename);
-		// if (isNumber(filenameBase)) {
-		// 	var breakpoint = filenameBase;
-		// 	_breakpoints.push(breakpoint);
-		// }
-
 		var layoutInfo = getLayoutInfo(filename)
 		if (layoutInfo) {
 			if (_layouts[layoutInfo.title]) {
@@ -194,17 +154,6 @@ module.exports = function ( grunt ) {
 		});
 	}
 
-
-	// function getHtmlForLayout(title, breakpoint) {
-	// 	var html = _htmlTemplate;
-	// 	var filenameBase = title + "@" + breakpoint;
-	// 	html = html.replace(/{{breakpoint}}/g, breakpoint);
-	// 	html = html.replace(/{{filenameBase}}/g, filenameBase);
-
-	// 	return html;
-	// }
-
-
 	function getHtml(title, breakpoints) {
 		var html = "";
 		var filenameBase = "";
@@ -218,33 +167,6 @@ module.exports = function ( grunt ) {
 
 		return html;
 	}
-
-
-	// function getLayoutHTML() {
-	// 	var html = "";
-
-	// 	for (var i = 0; i < _breakpoints.length; i++) {
-	// 		html += _htmlTemplate;
-	// 		html = html.replace(/{{breakpoint}}/g, _breakpoints[i]);
-	// 		html = html.replace(/{{filenameBase}}/g, _breakpoints[i]);
-	// 	}
-
-	// 	return html;
-	// }
-
-	// function getCssForLayout(title, breakpoint, breakpointIndex) {
-	// 	var css = _cssTemplate;
-	// 	var breakpointMin = 0;
-	// 	var breakpointMax = 0;
-
-	// 	breakpointMin = (breakpointIndex===0 ? 0 : breakpoint);
-	// 	breakpointMax = _layouts[title][breakpointIndex+1] - 1;
-	// 	css = css.replace(/{{breakpoint}}/g, _breakpoints[i]);
-	// 	css = css.replace(/{{breakpointMin}}/g, breakpointMin);
-	// 	css = css.replace(/{{breakpointMax}}/g, breakpointMax);
-		
-	// 	return css;
-	// }
 
 	function getCss(title, breakpoints) {
 		var css = "";
@@ -262,23 +184,6 @@ module.exports = function ( grunt ) {
 
 		return css;
 	}
-
-	// function getLayoutCSS() {
-	// 	var css = "";
-	// 	var breakpointMin = 0;
-	// 	var breakpointMax = 0;
-
-	// 	for (var i = 0; i < _breakpoints.length-1; i++) {
-	// 		breakpointMin = (i===0 ? 0 : _breakpoints[i]);
-	// 		breakpointMax = _breakpoints[i+1] - 1;
-	// 		css += _cssTemplate;
-	// 		css = css.replace(/{{breakpoint}}/g, _breakpoints[i]);
-	// 		css = css.replace(/{{breakpointMin}}/g, breakpointMin);
-	// 		css = css.replace(/{{breakpointMax}}/g, breakpointMax);
-	// 	}
-
-	// 	return css;
-	// }
 
 	function isNumber(string) {
 		if (string === "") {
